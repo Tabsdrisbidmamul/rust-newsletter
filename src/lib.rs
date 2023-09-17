6cbb45879d088894543ee4fa19f0c8bbfccb3279
Idris Khan <idris.khan303@gmail.comm>
2023-09-17 15:01:03 +0100

    started subscription endpoint and stub tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
-
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
 
 ///
 /// Health check endpoint, returns an empty 200 OK.
 /// This endpoint is used to poll the application with services like insights/ pingdom to ensure that the application is running for monitoring.
 ///
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+///
+/// Subscribe method which will take in POST'ed request body, extract user's email and name, save to db, and return a 200 back to client
+///
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 ///
@@ -15,9 +21,13 @@ async fn health_check() -> impl Responder {
 /// Will instantiate a new Server listening onto port 8000, this is only executed (lazy) when its awaited.
 ///
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health-check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health-check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
