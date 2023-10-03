f55f5229408ed4a20bf84fb393e43ecec4d5a3dc
Idris Khan <idris.khan303@gmail.comm>
2023-10-03 13:08:33 +0100

    Add application settings

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::net::TcpListener;
 
-use zero_to_production::startup::run;
+use zero_to_production::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port");
+    let config = get_configuration().expect("Failed to read configuration");
+
+    let address = format!("{}:{}", config.base_endpoint, config.application_port);
+
+    let listener = TcpListener::bind(address).expect("Failed to bind port");
+
     println!("listening on {:?}", listener.local_addr().unwrap());
 
     run(listener)?.await
