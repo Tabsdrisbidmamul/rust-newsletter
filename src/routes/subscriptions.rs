82233e9861b6a539ca36009abb05c4af88c3f624
Idris Khan <idris.khan303@gmail.comm>
2023-10-08 15:06:48 +0100

    WIP complete tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::{query, PgPool};
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize, Debug)]
@@ -21,20 +22,9 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
       subscriber_email = %form.email,
       subscriber_name = %form.name
     );
-
     let _request_span_guard = request_span.enter();
 
-    tracing::info!(
-        "request_id {}: Adding '{}' '{}' as a new subscriber",
-        request_id,
-        form.email,
-        form.name
-    );
-
-    tracing::info!(
-        "request_id {}: Saving new subscriber details in the database",
-        request_id
-    );
+    let query_span = tracing::info_span!("Saving new subscriber details in the database.");
 
     match query!(
         r#"
@@ -47,18 +37,10 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
         Utc::now()
     )
     .execute(db_pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {}: New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Created().finish()
-        }
-        Err(e) => {
-            tracing::error!("request_id {}: failed to execute query {:?}", request_id, e);
-            HttpResponse::InternalServerError().finish()
-        }
+        Ok(_) => HttpResponse::Created().finish(),
+        Err(e) => HttpResponse::InternalServerError().finish(),
     }
 }
