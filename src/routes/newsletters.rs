17e30bf5b1214f7176cf4ce4f27786d84cadb4c7
Idris Khan <idris.khan303@gmail.comm>
2024-01-13 17:48:32 +0000

    Rewrite validate credentials so timing of hashing function is consistent for incorrect username or password

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -214,19 +214,31 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let (user_id, expected_password_hash) = get_stored_credentials(&credentials.username, &pool)
-        .await
-        .map_err(PublishError::UnexpectedError)?
-        .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username")))?;
+    let mut user_id = None;
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$\
+  gZiV/M1gPc22ElAH/Jh1Hw$\
+  CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+            .to_string(),
+    );
+
+    if let Some((stored_user_id, stored_password_hash)) =
+        get_stored_credentials(&credentials.username, pool)
+            .await
+            .map_err(PublishError::UnexpectedError)?
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
 
     let _ = spawn_blocking_with_tracing(move || {
         verify_password_hash(expected_password_hash, credentials.password)
     })
     .await
     .context("Failed to spawn blocking task")
-    .map_err(PublishError::UnexpectedError)?;
+    .map_err(PublishError::UnexpectedError)??;
 
-    return Ok(user_id);
+    user_id.ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username")))
 }
 
 #[tracing::instrument(
@@ -238,7 +250,7 @@ fn verify_password_hash(
     password_candidate: Secret<String>,
 ) -> Result<(), PublishError> {
     // PHC format ${algorithm}${algorithm version}${,-separated algorithm parameters}${hash}${salt}
-    let expected_password_hash = PasswordHash::new(&expected_password_hash.expose_secret())
+    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
         .context("Failed to parse hash in PHC string format")
         .map_err(PublishError::UnexpectedError)?;
 
