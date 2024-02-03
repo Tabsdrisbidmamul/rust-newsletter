3cc7026f6657a5f616ebaef9990cf991802b3d87
Idris Khan <idris.khan303@gmail.comm>
2024-02-03 16:38:21 +0000

    updated tests on change password feat

--- src/routes/dashboard/password/post.rs
+++ src/routes/dashboard/password/post.rs
@@ -1,8 +1,14 @@
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
+use sqlx::PgPool;
 
-use crate::helpers::see_other;
+use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    helpers::{e500, see_other},
+    routes::get_username,
+    session_state::TypedSession,
+};
 
 #[derive(serde::Deserialize)]
 pub struct ChangePasswordForm {
@@ -13,15 +19,63 @@ pub struct ChangePasswordForm {
 
 pub async fn change_password(
     form: web::Form<ChangePasswordForm>,
+    session: TypedSession,
+    pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
-        FlashMessage::error(
-            "You entered two different new passwords - the field values must match.",
-        )
-        .send();
+    let user_id = session.get_user_id().map_err(e500)?;
 
-        return Ok(see_other("/admin/password"));
+    if user_id.is_none() {
+        return Ok(see_other("/login"));
+    }
+
+    let user_id = user_id.unwrap();
+    let current_password = form.0.current_password.expose_secret();
+    let new_password = form.0.new_password.expose_secret();
+    let new_password_check = form.0.new_password_check.expose_secret();
+
+    if check_if_password_match(&new_password, &new_password_check) {
+        return Ok(send_flash_message_and_redirect(
+            "You entered two different new passwords - the field values must match.",
+            "/admin/password",
+        ));
+    }
+
+    if check_if_password_length_is_greater_than_12(&new_password) {
+        return Ok(send_flash_message_and_redirect(
+            "The password provided is too short, passwords must be greater than 12 characters",
+            "/admin/password",
+        ));
+    }
+
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
+
+    let credentials = Credentials {
+        username,
+        password: form.0.current_password,
+    };
+
+    if let Err(e) = validate_credentials(credentials, &pool).await {
+        return match e {
+            AuthError::InvalidCredentials(_) => {
+                FlashMessage::error("The current password is incorrect").send();
+                Ok(see_other("/admin/password"))
+            }
+            AuthError::UnexpectedError(_) => Err(e500(e).into()),
+        };
     }
 
     todo!()
+}
+
+fn send_flash_message_and_redirect(flash_message: &str, redirect: &str) -> HttpResponse {
+    FlashMessage::error(flash_message.to_string()).send();
+    return see_other(redirect);
+}
+
+fn check_if_password_length_is_greater_than_12(password: &str) -> bool {
+    password.len() < 12 || password.len() > 129
+}
+
+fn check_if_password_match(new_password: &str, new_password_check: &str) -> bool {
+    new_password != new_password_check
 }
--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -1,3 +1,4 @@
+use rstest::rstest;
 use uuid::Uuid;
 
 use crate::helpers::{assert_is_redirect_to, spawn_app};
@@ -65,4 +66,70 @@ async fn new_password_fields_must_match() {
     assert!(html_page.contains(
         "<p><i>You entered two different new passwords - the field values must match.</i></p>"
     ));
+}
+
+#[tokio::test]
+async fn current_password_must_be_valid() {
+    // Arrange
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let wrong_password = Uuid::new_v4().to_string();
+
+    // Act - p1 login
+    app.post_login(&serde_json::json!({
+      "username": &app.test_user.username,
+      "password": &app.test_user.password
+    }))
+    .await;
+
+    // Act - p2 try to change password
+    let response = app
+        .post_change_password(&serde_json::json!({
+          "current_password": &wrong_password,
+          "new_password": &new_password,
+          "new_password_check": &new_password,
+        }))
+        .await;
+
+    // Assert
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // Act - p3 follow the redirect (acts as the refresh when submitting the form)
+    let html_page = app.get_change_password_html().await;
+
+    assert!(html_page.contains("<p><i>The current password is incorrect</i></p>"));
+}
+
+#[rstest]
+#[case("Password")]
+#[case("1.391cwypz")]
+#[case("1.h10pxkzy")]
+#[tokio::test]
+async fn new_password_is_too_short(#[case] password: String) {
+    // Arrange
+    let app = spawn_app().await;
+
+    // Act - p1 login
+    app.post_login(&serde_json::json!({
+      "username": &app.test_user.username,
+      "password": &app.test_user.password
+    }))
+    .await;
+
+    // Act - p2 try to change password (cases where password are less than 12 characters)
+    let response = app
+        .post_change_password(&serde_json::json!({
+          "current_password": &app.test_user.password,
+          "new_password": password,
+          "new_password_check": password,
+        }))
+        .await;
+
+    // Assert
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // Act - p3 follow the redirect (acts as the refresh when submitting the form)
+    let html_page = app.get_change_password_html().await;
+
+    assert!(html_page.contains("<p><i>The password provided is too short, passwords must be greater than 12 characters</i></p>"));
 }
